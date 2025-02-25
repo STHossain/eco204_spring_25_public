52c24c0e5de32ebd7642c575f48d7c5944e395c2
Tanvir <tanvirhossain.sk@gmail.com>
2025-02-25 21:33:24 +0600

    fixed the mistake in the solutions

--- Problem Sets/PS_0/Solutions/PS_0_3.R
+++ Problem Sets/PS_0/Solutions/PS_0_3.R
@@ -4,12 +4,15 @@
 #==============================================================================
 
 
+
+
+
 # question 3
 #--------------------
 # generate random numbers from the normal distribution with mean 10 and variance 2 and save in data
 
-n <- 100 # set the sample size
-data <- rnorm(n)
+sample_size <- 100 # set the sample size
+data <- rnorm(n = sample_size, mean = 10, sd = sqrt(2))
 
 # plot the histogram
 hist(data)
@@ -23,7 +26,7 @@ str(hist(data))
 #--------------------
 # We need to calculate the probability P(X <= 21), when X is a random variable from the normal distribution with mean 20 and variance 3.
 
-pnorm(21, mean = 20, sd = sqrt(3))
+pnorm(q = 21, mean = 20, sd = sqrt(3))
 # the answer is 0.7181486
 
 
@@ -32,7 +35,9 @@ pnorm(21, mean = 20, sd = sqrt(3))
 #--------------------
 # here we need to calculate .95 quantile or 95th percentile of the normal distribution with mean 20 and variance 3, 
 
-qnorm(0.95, mean = 20, sd = sqrt(3))
+
+
+qnorm(p = 0.95, mean = 20, sd = sqrt(3))
 # the answer is 22.84897
 
 
